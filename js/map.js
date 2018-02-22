49024df347a7cccf9630e7413660db17a3a537b1
Daria <mincedhamster@gmail.com>
2018-02-22 23:45:27 +0300

    Исправила маркеры

--- js/map.js
+++ js/map.js
@@ -9,29 +9,29 @@ zoom: 11
 
 myPlacemark1 = new ymaps.Placemark([59.97789581, 30.30915326], null, {
 	iconLayout: "default#image",
-	iconImageHref: "../img/icons/map-marker.png",
-	iconImageSize: [40, 40],
+	iconImageHref: "img/icons/map-marker.png",
+	iconImageSize: [50, 50],
 	iconImageOffset: [-20, -20],
 });
 
 myPlacemark2 = new ymaps.Placemark([59.86862558, 30.33181256], null, {
 	iconLayout: "default#image",
-	iconImageHref: "../img/icons/map-marker.png",
-	iconImageSize: [40, 40],
+	iconImageHref: "img/icons/map-marker.png",
+	iconImageSize: [50, 50],
 	iconImageOffset: [-20, -20],
 });
 
 myPlacemark3 = new ymaps.Placemark([59.92729397, 30.49105452], null, {
 	iconLayout: 'default#image',
-	iconImageHref: "../img/icons/map-marker.png",
-	iconImageSize: [40, 40],
+	iconImageHref: "img/icons/map-marker.png",
+	iconImageSize: [50, 50],
 	iconImageOffset: [-20, -20],
 });
 
 myPlacemark4 = new ymaps.Placemark([59.94788638, 30.38836452], null, {
 	iconLayout: "default#image",
-	iconImageHref: "../img/icons/map-marker.png",
-	iconImageSize: [40, 40],
+	iconImageHref: "img/icons/map-marker.png",
+	iconImageSize: [50, 50],
 	iconImageOffset: [-20, -20],
 });
 myMap.geoObjects.add(myPlacemark1);
